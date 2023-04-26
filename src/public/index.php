43d29669d62371daa581cf80878f38f5255243cf
mikan19 <mellamo.rina.ty@gmail.com>
2023-04-26 18:01:52 +0900

    fix:3の倍数の値うち最大値を出力するコードに修正しました

--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,29 @@
 <?php
-echo 'Welcome TECH QUEST!';
+$arrayList = [
+	[3, 5, 1, 0, 9, 12],
+	[1, 2, 14, 10, 19, 1],
+	[0, 3, 3, 3, 7, 2]
+];
+foreach ($arrayList as $array) {
+    $arrayThree = [];
+    foreach ($array as $num){
+        if($num % 3 == 0){
+            $arrayThree[] = $num;
+        }
+    }
+    if(empty($arrayThree)){
+        $max_value = max($array);
+    }else{
+        $max_value = max($arrayThree);
+    } 
+    echo $max_value. "\n";
+
+}
+
+
+
+
+
+
+
+
